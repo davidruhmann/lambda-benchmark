39d87c870adbb070da024b8bd7f41ef5ea35f17b
David Ruhmann <davidruhmann@gmail.com>
2024-02-01 16:22:04 +0000

    feat: add dotnet8 aot al2023 runtime

---
+++ runtimes/dotnet8al2023/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $1 = "x86_64" ]; then
+    ARCH="x64"
+    IMAGE_ARCH="amd64"
+elif [ $1 = "arm64" ]; then
+    ARCH="arm64"
+    IMAGE_ARCH="arm64v8"
+fi
+
+path=$(sed -n 's/path: "\(.*\)"/\1/p' manifest.yml)
+zip="${path}_${ARCH}.zip"
+
+rm ${zip} 2> /dev/null
+
+docker build . --build-arg ARCH=${ARCH} --build-arg IMAGE_ARCH=${IMAGE_ARCH} -t mbwilding/dotnet8al2023
+dockerId=$(docker create mbwilding/dotnet8al2023)
+docker cp $dockerId:/code.zip ${zip}
