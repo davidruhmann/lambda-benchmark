a9c11b860c53f55866be31222c64d57d7e6ff59a
David Ruhmann <druhmann@redventures.com>
2024-02-01 19:43:59 -0500

    chore: add manual gha

--- runtimes/dotnet8al2023/build.sh
+++ runtimes/dotnet8al2023/build.sh
@@ -3,9 +3,11 @@
 if [ $1 = "x86_64" ]; then
     ARCH="x64"
     IMAGE_ARCH="amd64"
+    PLATFORM="linux/amd64"
 elif [ $1 = "arm64" ]; then
     ARCH="arm64"
     IMAGE_ARCH="arm64v8"
+    PLATFORM="linux/arm64"
 fi
 
 path=$(sed -n 's/path: "\(.*\)"/\1/p' manifest.yml)
@@ -13,6 +15,8 @@ zip="${path}_${ARCH}.zip"
 
 rm ${zip} 2> /dev/null
 
-docker build . --build-arg ARCH=${ARCH} --build-arg IMAGE_ARCH=${IMAGE_ARCH} -t mbwilding/dotnet8al2023
+#docker buildx create --name container --driver=docker-container
+docker buildx build . --builder=container --platform=${PLATFORM} --build-arg ARCH=${ARCH} --build-arg IMAGE_ARCH=${IMAGE_ARCH} -t mbwilding/dotnet8al2023 --load
+#docker build . --platform=${PLATFORM} --build-arg ARCH=${ARCH} --build-arg IMAGE_ARCH=${IMAGE_ARCH} -t mbwilding/dotnet8al2023
 dockerId=$(docker create mbwilding/dotnet8al2023)
 docker cp $dockerId:/code.zip ${zip}
--- runtimes/dotnet8al2023/src/Function.cs
+++ runtimes/dotnet8al2023/src/Function.cs
@@ -1,6 +1,7 @@
 using Amazon.Lambda.RuntimeSupport;
-using Amazon.Lambda.Serialization.SystemTextJson;
-using System.Text.Json.Serialization;
+using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 namespace Lambda;
 
@@ -8,21 +9,36 @@ public class Function
 {
     private static async Task Main()
     {
-        await LambdaBootstrapBuilder.Create(FunctionHandler, new SourceGeneratorLambdaJsonSerializer<LambdaFunctionJsonSerializerContext>())
+        await LambdaBootstrapBuilder.Create(FunctionHandler)
             .Build()
             .RunAsync();
     }
 
-    public static StatusResponse FunctionHandler()
+    public static async Task FunctionHandler(ILambdaContext context)
     {
-        return new StatusResponse(StatusCode: 200);
+        var iterations = Convert.ToInt32(Environment.GetEnvironmentVariable("ITERATIONS_CODE"));
+        var bucketName = Environment.GetEnvironmentVariable("BUCKET_NAME");
+        var bucketKey = $"test/{context.FunctionName}/test.txt";
+
+        var s3 = new AmazonS3Client();
+
+        for (var i = 0; i < iterations; i++)
+        {
+            var request = new PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = bucketKey,
+                ContentType = "text/plain",
+                ContentBody = i.ToString(),
+            };
+
+            await s3.PutObjectAsync(request).ConfigureAwait(false);
+        }
+
+        await s3.DeleteObjectAsync(new DeleteObjectRequest
+        {
+            BucketName = bucketName,
+            Key = bucketKey
+        }).ConfigureAwait(false);
     }
-}
-
-public record StatusResponse(int StatusCode);
-
-[JsonSerializable(typeof(StatusResponse))]
-[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
-public partial class LambdaFunctionJsonSerializerContext : JsonSerializerContext
-{
 }
